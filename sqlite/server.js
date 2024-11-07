0d9780891f1ddb256a190cccaf4eefc3c2da9625
Sebastian Quiros <sebastianq04@gmail.com>
2024-11-07 01:49:19 -0500

    Implemented basic insertData SQL code from Sajad's repo

--- sqlite/server.js
+++ sqlite/server.js
@@ -1,21 +1,11 @@
 // Import sqlite3 package
 const sqlite3 = require('sqlite3');
-// Create or connect to an SQLite database
-const db = new sqlite3.Database('./databases/test.db', (err) => {
-  if (err) {
-    console.error('Error opening database:', err.message);
-  } else {
-    console.log('Connected to the SQLite database.');
-  }
+const sqllib = require('./sql.js');
+
+var result = sqllib.insertData("test", "asdas", {
+  "id": 1234567,
+  "name": "Bobby",
+  "email": "bob@bob.com"
 });
 
-var sql = "INSERT INTO users(id, name, email) VALUES(?, ?, ?)"
-
-db.run(sql, [1234, "John", "john@doe.com"], function(err) {
-    if (err) {
-        console.log(err);
-    }
-});
-
-db.close()
-
+console.log(result)
--- sqlite/sql.js
+++ sqlite/sql.js
@@ -1,29 +1,75 @@
+const sqlite3 = require('sqlite3');
+
 module.exports = {
-    startConn
+    insertData
 }
 
 function startConn (DBname) {
     // Create or connect to an SQLite database
     var db = new sqlite3.Database('./databases/' + DBname + ".db", (err) => {
     if (err) {
-      console.error('Error opening database:', err.message);
-    } else {
         return {
-            "db": db,
-            "response": 'Connected to the SQLite database.'
+            "db": null,
+            "response": err.message
         }
     }
   });
+  return {
+    "db": db,
+    "response": 'Connected to the SQLite database.'
+  }
+}
+
+function insertData(DBname, table, info)
+{
+    var response = startConn(DBname);
+    console.log(response);
+    var db = response["db"];
+    if (db == null) return response["response"];
+
+    var keys = Object.keys(info);
+    var values = Object.values(info);
+    var sql = "INSERT INTO " + table + " (";
+    for (let i = 0; i < keys.length; i++) {
+        sql += keys[i];
+        if (i != keys.length - 1) {
+        sql += ", ";
+        } else {
+        sql += ") VALUES (";
+        }
+    }
+
+    for (let i = 0; i < keys.length; i++) {
+        sql += "'" + values[i] + "'";
+        if (i != keys.length - 1) {
+        sql += ", ";
+        } else {
+        sql += ")";
+        }
+    }
+
+    db.run(sql, [], function(err) {
+        if (err) {
+            db.close()
+            return(err.message);
+        }
+    });
+
+    db.close()
+    return("Success!");
 }
 
 
-var sql = "INSERT INTO users(id, name, email) VALUES(?, ?, ?)"
-
-db.run(sql, [1234, "John", "john@doe.com"], function(err) {
-    if (err) {
-        console.log(err);
-    }
-});
-
-db.close()
-
+/*
+async function getTable(table) {
+    const [rows] = await con.query("SELECT * FROM " + table);
+    return rows;
+  }
+  
+  async function deleteRecord(info, table) {
+    var sql = "DELETE FROM " + table + " WHERE id = ? AND password = ?";
+    const result = await con.query(sql, [info.id, info.password]);
+    console.log("Number of records deleted: " + result[0].affectedRows);
+    console.log(await getTable(table));
+  }
+    */
