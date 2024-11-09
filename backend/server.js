21ee729c510aa096e7dfbed2b99ebe6e68eb712b
Sajad <s.b.bayat78@gmail.com>
2024-11-08 23:09:31 -0500

    added Sign up funcs

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,39 @@
-const sqlite3 = require('sqlite3');
-const sqllib = require('./sql.js');
+const sqlite3 = require("sqlite3");
+const {
+  insertInto,
+  getTable,
+  getRow,
+  updateRow,
+  deleteRow,
+} = require("./sql.js");
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const PORT = 3000;
-app.use(cors())
+const PORT = 3010;
+app.use(cors());
 
-app.post("/signup/", (req,res) => {
-
-  
+app.get("/signup/", (req, res) => {
+  const user = req.query;
+  const result = insertInto("./databases/main.db", "users", user);
+  const resultArr = result.split(":");
+  // console.log(resultArr);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  if (resultArr[0] == "UNIQUE constraint failed") {
+    console.log("choose a different " + resultArr[1].split(".")[1]);
+    res.send({ result: "choose a different " + resultArr[1].split(".")[1] });
+  }
+  console.log(result);
+  res.send({ result: "user added" });
 });
+// deleteRow("./databases/main.db", "users", { uid: 1 });
+// console.log(getTable("./databases/main.db", "users"));
 
-app.post("/signin/", (req,res) => {
-
-});
+app.post("/signin/", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log("Listening on port " + PORT);
-})
+});
 
 /* Sample getTable
 sqllib.getTable("test", "users").then(function(result){
@@ -37,4 +52,3 @@ sqllib.insertData("test", "users", {
   console.log(err);
 });
 */
-
